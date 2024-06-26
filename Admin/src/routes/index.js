5874442d72a7e6920069354a9a433c0f3fa84cd9
Emiliano <107131046+olverase@users.noreply.github.com>
2024-06-26 10:54:55 -0600

    Update index.js

--- Admin/src/routes/index.js
+++ Admin/src/routes/index.js
@@ -2,3 +2,5 @@ export { Root } from "./Root";
 export { ErrorPage } from "./ErrorPage/ErrorPage";
 export { Index, loader as indexLoader } from "./Index/Index";
 export { BookInfo, loader as bookInfoLoader } from "./BookInfo/BookInfo";
+export * from './AgregarLibro/AgregarLibro'
+export * from './EditarLibro/EditarLibro'
