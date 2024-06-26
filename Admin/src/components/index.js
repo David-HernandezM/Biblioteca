430e3873e6fc89e41bd2a81a63aefb376a8f6760
Emiliano <107131046+olverase@users.noreply.github.com>
2024-06-26 11:04:33 -0600

    Update index.js

--- Admin/src/components/index.js
+++ Admin/src/components/index.js
@@ -1,11 +1,4 @@
-export { NavOption } from "./NavOption/NavOption";
-export { NavOptions } from "./NavOptions/NavOptions";
-export { Header } from "./layout";
-export { SearchBar } from "./SearchBar/SearchBar";
-export { BookCard } from "./BookCard/BookCard";
-export { MessageModal } from "./MessageModal/MessageModal";
-export { 
-    ReserveBookModalMessage,
-    UserLoginMessage
-} from "./ModalMessages";
-export { NoBooksMessage } from "./NoBooksMessage/NoBooksMessage";
+export * from './MessageModal/MessageModal'
+export * from './ModalMessages'
+export * from './layout'
+export * from './MediaCard/MediaCard'
